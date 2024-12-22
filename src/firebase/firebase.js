b2595e46cbe35d75a5f0ec0774ec4c55569edcc2
manubg <bgmanu2426@gmail.com>
2024-12-22 07:55:14 +0530

    Refactor Firebase configuration to use environment variables and add sample environment file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore"
+import config from "@/utils/config";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA8e57MngvrGi5dXbXOXmkVodNhJ0_tw84",
-    authDomain: "next-chat-app-89891.firebaseapp.com",
-    projectId: "next-chat-app-89891",
-    storageBucket: "next-chat-app-89891.appspot.com",
-    messagingSenderId: "916868416986",
-    appId: "916868416986:web:9f58274f5546c471306ce3"
+    apiKey: config.firebaseConfig.apiKey,
+    authDomain: config.firebaseConfig.authDomain,
+    projectId: config.firebaseConfig.projectId,
+    storageBucket: config.firebaseConfig.storageBucket,
+    messagingSenderId: config.firebaseConfig.messagingSenderId,
+    appId: config.firebaseConfig.appId
 };
 
 // Initialize Firebase
